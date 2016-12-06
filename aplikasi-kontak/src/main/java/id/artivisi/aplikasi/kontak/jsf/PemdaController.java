463bbe25c012db87226136eac75f84098755bc1b
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-06 15:14:57 +0700

    implement form input

--- aplikasi-kontak/src/main/java/id/artivisi/aplikasi/kontak/jsf/PemdaController.java
+++ aplikasi-kontak/src/main/java/id/artivisi/aplikasi/kontak/jsf/PemdaController.java
@@ -5,14 +5,27 @@ import id.artivisi.aplikasi.kontak.sb.PemdaFacade;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.inject.Named;
-import javax.enterprise.context.Dependent;
+import javax.enterprise.context.RequestScoped;
 
 @Named(value = "pemdaController")
-@Dependent
+@RequestScoped
 public class PemdaController {
 
     @EJB
     private PemdaFacade pemdaFacade;
+    
+    private Pemda pemda = new Pemda();
+
+    public Pemda getPemda() {
+        return pemda;
+    }
+    
+    public String simpanPemda(){
+        pemdaFacade.create(pemda);
+        pemda = new Pemda();
+        
+        return "list?faces-redirect=true";
+    }
 
     public List<Pemda> daftarPemda(){
         return pemdaFacade.findAll();
